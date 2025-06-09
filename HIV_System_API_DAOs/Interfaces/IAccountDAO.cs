4b52925b8617e90d425d497e2a351dbc70d69d86
haibeu2901 <beu2901@gmail.com>
2025-06-09 23:39:41 +0700

    init interface account dao

---
+++ HIV_System_API_DAOs/Interfaces/IAccountDAO.cs
@@ -0,0 +1,15 @@
+﻿using HIV_System_API_BOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIV_System_API_DAOs.Interfaces
+{
+    public interface IAccountDAO
+    {
+        Task<List<Account>> GetAllAccountsAsync();
+        Task<Account> GetAccountByLoginAsync(string accUsername, string accPassword);
+    }
+}
