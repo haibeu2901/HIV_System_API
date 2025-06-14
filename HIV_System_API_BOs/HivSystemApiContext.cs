5495824b668c29ff17617d79d71a4215b88a07aa
haibeu2901 <beu2901@gmail.com>
2025-06-14 16:48:16 +0700

    modify Context to get db config string

--- HIV_System_API_BOs/HivSystemApiContext.cs
+++ HIV_System_API_BOs/HivSystemApiContext.cs
@@ -54,7 +54,7 @@ public partial class HivSystemApiContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 // To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=localhost;Database=HIV_SYSTEM_API;Uid=sa;Pwd=12345;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer(GetConnectionString());
 
     private string? GetConnectionString()
     {
