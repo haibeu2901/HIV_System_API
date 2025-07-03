bd938bfa92b4295b25b53df748af41e577bff01a
Tran Duc An <faneverdie2005@gmail.com>
2025-07-04 03:25:19 +0700

    Add payment method using Stripe

---
+++ HIV_System_API_DTOs/PaymentDTO/PaymentRequestDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIV_System_API_DTOs.PaymentDTO
+{
+    public class PaymentRequestDTO
+    {
+        public decimal Amount { get; set; }
+        public string? Currency { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ HIV_System_API_Services/Interfaces/IPaymentService.cs
@@ -0,0 +1,15 @@
+﻿using HIV_System_API_DTOs.PaymentDTO;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HIV_System_API_Services.Interfaces
+{
+    public interface IPaymentService
+    {
+        Task<PaymentIntent> CreatePaymentIntent(PaymentRequestDTO request);
+    }
+}
